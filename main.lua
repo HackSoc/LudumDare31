f7453c97e7e3e07cddb7ebfecd0c94eb68ee893b
David Morris <davidrowlandmorris@gmail.com>
2014-12-06 03:34:12 +0000

    Update all entities

--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
+local entities = {}
+
 function love.load()
     -- initialisation code goes here
 end
 
 function love.update(dt)
-    -- simulate the passing of time---<dt> is the
-    -- time (fraction of a second) that has passed
-    -- since the last call to love.update
+    for _, e in pairs(entites) do
+        e:update(dt)
+    end
 end
 
 function love.draw()
