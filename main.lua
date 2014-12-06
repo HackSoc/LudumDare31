382ec9376d670225e7bc3563b8baf57b501ba3c7
David Morris <davidrowlandmorris@gmail.com>
2014-12-06 02:42:19 +0000

    Skeleton main.lua

---
+++ main.lua
@@ -0,0 +1,29 @@
+function love.load()
+    -- initialisation code goes here
+end
+
+function love.update(dt)
+    -- simulate the passing of time---<dt> is the
+    -- time (fraction of a second) that has passed
+    -- since the last call to love.update
+end
+
+function love.draw()
+    -- draw the current frame
+end
+
+function love.keypressed(key)
+    -- handle <key> being pressed
+end
+
+function love.keyreleased(key)
+    -- handle <key> being released
+end
+
+function love.mousepressed(x, y, button)
+    -- handle mouse presses
+end
+
+function love.mousereleased(x, y, button)
+    -- handle mouse button releases
+end
